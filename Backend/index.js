98c0849fcfc39ef45929de5454df36b9d16b2e96
Pushkar-Garg <167011407+Pushkar-Garg@users.noreply.github.com>
2024-04-29 21:04:56 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -119,15 +119,10 @@ app.post('/signup', async (req, res) => {
         if (check) {
             return res.status(400).json({ success: success, errors: "existing user found with this email" });
         }
-        let cart = {};
-          for (let i = 0; i < 300; i++) {
-          cart[i] = 0;
-        }
         const user = new Users({
             name: req.body.username,
             email: req.body.email,
             password: req.body.password,
-            cartData: cart,
         });
         await user.save();
         const data = {
